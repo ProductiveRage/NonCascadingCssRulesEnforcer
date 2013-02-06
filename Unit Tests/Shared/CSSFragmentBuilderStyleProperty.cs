7bb9fb5e0a9c1c5baa170bb9766a6bc7d3f7b466
dan <dan@newmind.co.uk>
2013-02-06 23:41:10 +0000

    BodyScopingMustBeAppliedToNonResetsOrThemesSheets corrected to ensure that there are no style properties associated with the body tag (in which case it is not solely for scope-restriction) - Some common investigation methods moved to become extension classes (eg. Selector.IsScopeRestrictingBodyTag()) - Some very basic unit tests added for the two current rules (convenience classes exist in the Unit Tests project to make constructing ICSSFragment data simpler)

---
+++ Unit Tests/Shared/CSSFragmentBuilderStyleProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UnitTests.Shared
+{
+	public class CSSFragmentBuilderStyleProperty : ICSSFragmentBuilderComponent
+	{
+		public static CSSFragmentBuilderStyleProperty New(string name, string value)
+		{
+			return new CSSFragmentBuilderStyleProperty(name, value);
+		}
+
+		private CSSFragmentBuilderStyleProperty(string name, string value)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("Null/blank name specified");
+			if (string.IsNullOrWhiteSpace(value))
+				throw new ArgumentException("Null/blank value specified");
+
+			Name = name;
+			Value = value;
+		}
+
+		/// <summary>
+		/// This will never be null or empty
+		/// </summary>
+		public string Name { get; private set; }
+
+		/// <summary>
+		/// This will never be null or empty
+		/// </summary>
+		public string Value { get; private set; }
+	}
+}

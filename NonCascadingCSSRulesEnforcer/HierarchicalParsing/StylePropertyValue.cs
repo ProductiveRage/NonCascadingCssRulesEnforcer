ae64e35536593292d2294e4d02eaf54934225bbe
dan <742348+ProductiveRage@users.noreply.github.com>
2013-04-17 19:40:31 +0100

    Added AllMeasurementsMustBePixels rule - Updated CSSParser - Changed build  to x86 for now to prevent warning about mismatch with CSSParser - Added TopLevel and Compiled StyleSheetOptions as they will be more applicable for future rules

--- NonCascadingCSSRulesEnforcer/HierarchicalParsing/StylePropertyValue.cs
+++ NonCascadingCSSRulesEnforcer/HierarchicalParsing/StylePropertyValue.cs
@@ -1,7 +1,20 @@
-﻿namespace NonCascadingCSSRulesEnforcer.HierarchicalParsing
+﻿using System;
+
+namespace NonCascadingCSSRulesEnforcer.HierarchicalParsing
 {
 	public class StylePropertyValue : StyleProperty
 	{
-		public StylePropertyValue(string value, int sourceLineIndex) : base(value, sourceLineIndex) { }
+		public StylePropertyValue(StylePropertyName property, string value, int sourceLineIndex) : base(value, sourceLineIndex)
+		{
+			if (property == null)
+				throw new ArgumentNullException("property");
+
+			Property = property;
+		}
+
+		/// <summary>
+		/// This will never be null
+		/// </summary>
+		public StylePropertyName Property { get; private set; }
 	}
 }
---
+++ Unit Tests/Rules/AllMeasurementsMustBePixelsTests.cs
@@ -0,0 +1,93 @@
+﻿using NonCascadingCSSRulesEnforcer.HierarchicalParsing;
+using NonCascadingCSSRulesEnforcer.Rules;
+using UnitTests.Shared;
+using Xunit;
+
+namespace UnitTests.Rules
+{
+	public class AllMeasurementsMustBePixelsTests
+	{
+		[Fact]
+		public void PercentageWidthDivsMayBeAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("width", "50%")
+			).ToSelector();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new AllMeasurementsMustBePixels(AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs)).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void HundredPercentageWidthImgsMayBeAcceptableIfNestedInPercentageWidthDivs()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("width", "50%"),
+				CSSFragmentBuilderSelector.New(
+					"img",
+					CSSFragmentBuilderStyleProperty.New("width", "100%")
+				)
+			).ToSelector();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new AllMeasurementsMustBePixels(AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs)).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void HundredPercentageWidthImgsMayBeAcceptableIfDeeplyNestedInPercentageWidthDivs()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("width", "50%"),
+				CSSFragmentBuilderSelector.New(
+					"p",
+					CSSFragmentBuilderSelector.New(
+						"img",
+						CSSFragmentBuilderStyleProperty.New("width", "100%")
+					)
+				)
+			).ToSelector();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new AllMeasurementsMustBePixels(AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs)).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void HundredPercentageWidthImgsAreNotAcceptableIfNotNestedInPercentageWidthDivs()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"img",
+				CSSFragmentBuilderStyleProperty.New("width", "100%")
+			).ToSelector();
+
+			Assert.Throws<AllMeasurementsMustBePixels.AllMeasurementsMustBePixelsNotAppliedException>(
+				() => (new AllMeasurementsMustBePixels(AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs)).EnsureRulesAreMet(new[] { content })
+			);
+		}
+
+		[Fact]
+		public void NonHundredPercentageWidthImgsAreNotAcceptableIfNestedInPercentageWidthDivs()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("width", "50%"),
+				CSSFragmentBuilderSelector.New(
+					"img",
+					CSSFragmentBuilderStyleProperty.New("width", "80%")
+				)
+			).ToSelector();
+
+			Assert.Throws<AllMeasurementsMustBePixels.AllMeasurementsMustBePixelsNotAppliedException>(
+				() => (new AllMeasurementsMustBePixels(AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs)).EnsureRulesAreMet(new[] { content })
+			);
+		}
+	}
+}

3a96eec6b0bff64d3f75909001535e7461ec7e1c
dan <742348+ProductiveRage@users.noreply.github.com>
2013-04-23 00:10:44 +0100

    Added MarginsMustBeFullySpecifiedIfSpecifiedAtAll rule

---
+++ Unit Tests/Rules/MarginsMustBeFullySpecifiedIfSpecifiedAtAllTests.cs
@@ -0,0 +1,80 @@
+﻿using NonCascadingCSSRulesEnforcer.Rules;
+using UnitTests.Shared;
+using Xunit;
+
+namespace UnitTests.Rules
+{
+	public class MarginsMustBeFullySpecifiedIfSpecifiedAtAllTests
+	{
+		[Fact]
+		public void NoMarginAtAllIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New("div").ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new MarginsMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void MarginFullyDefinedIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("margin", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new MarginsMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void MarginExplicitlyFullyDefinedIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("margin-top", "16px"),
+				CSSFragmentBuilderStyleProperty.New("margin-left", "16px"),
+				CSSFragmentBuilderStyleProperty.New("margin-bottom", "16px"),
+				CSSFragmentBuilderStyleProperty.New("margin-right", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new MarginsMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void CombinationOfMarginAndMarginTopIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("margin-top", "8px"),
+				CSSFragmentBuilderStyleProperty.New("margin", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new MarginsMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void MarginTopOnlyIsNotAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("margin-top", "16px")
+			).ToContainerFragment();
+
+			Assert.Throws<MarginsMustBeFullySpecifiedIfSpecifiedAtAll.MarginsMustBeFullySpecifiedIfSpecifiedAtAllException>(() =>
+			{
+				(new MarginsMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+	}
+}

7610ee814af160b6a543ad21de030a6e11c6c33d
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2013-04-23 11:47:34 +0100

    Extracted core of MarginMustBeFullySpecifiedIfSpecifiedAtAll into a base class so the logic can be shared to cover margin and padding without duplication (will also be used for border at some point)

---
+++ NonCascadingCSSRulesEnforcer/Rules/PaddingMustBeFullySpecifiedIfSpecifiedAtAll.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+using CSSParser.ExtendedLESSParser;
+
+namespace NonCascadingCSSRulesEnforcer.Rules
+{
+	/// <summary>
+	/// This rule applies to all stylesheets. If any padding is specified, the padding must be fully specified - eg. "padding: 16px" is fine, "padding: 16px; padding-top: 8px"
+	/// is fine, "padding-top: 8px; padding-left: 16px; padding-bottom: 16px; padding-right: 16px" is fine, "padding-left: 16" with the others unspecified is not.
+	/// </summary>
+	public class PaddingMustBeFullySpecifiedIfSpecifiedAtAll : PropertyMustBeFullySpecifiedIfSpecifiedAtAll
+	{
+		public PaddingMustBeFullySpecifiedIfSpecifiedAtAll()
+			: base(
+				new[] { "padding-top" },
+				new[] { "padding-left" },
+				new[] { "padding-bottom" },
+				new[] { "padding-right" },
+				new[] { "padding" },
+				fragment => new PaddingMustBeFullySpecifiedIfSpecifiedAtAllException(fragment)
+			) { }
+			
+
+		public class PaddingMustBeFullySpecifiedIfSpecifiedAtAllException : BrokenRuleEncounteredException
+		{
+			public PaddingMustBeFullySpecifiedIfSpecifiedAtAllException(ICSSFragment fragment) : base("Style block encountered with incomplete padding specification", fragment) { }
+			protected PaddingMustBeFullySpecifiedIfSpecifiedAtAllException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+		}
+	}
+}
---
+++ Unit Tests/Rules/PaddingMustBeFullySpecifiedIfSpecifiedAtAllTests.cs
@@ -0,0 +1,80 @@
+﻿using NonCascadingCSSRulesEnforcer.Rules;
+using UnitTests.Shared;
+using Xunit;
+
+namespace UnitTests.Rules
+{
+	public class PaddingMustBeFullySpecifiedIfSpecifiedAtAllTests
+	{
+		[Fact]
+		public void NoPaddingAtAllIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New("div").ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void PaddingFullyDefinedIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("padding", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void PaddingExplicitlyFullyDefinedIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("padding-top", "16px"),
+				CSSFragmentBuilderStyleProperty.New("padding-left", "16px"),
+				CSSFragmentBuilderStyleProperty.New("padding-bottom", "16px"),
+				CSSFragmentBuilderStyleProperty.New("padding-right", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void CombinationOfPaddingAndPaddingTopIsAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("padding-top", "8px"),
+				CSSFragmentBuilderStyleProperty.New("padding", "16px")
+			).ToContainerFragment();
+
+			Assert.DoesNotThrow(() =>
+			{
+				(new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+
+		[Fact]
+		public void PaddingTopOnlyIsNotAcceptable()
+		{
+			var content = CSSFragmentBuilderSelector.New(
+				"div",
+				CSSFragmentBuilderStyleProperty.New("padding-top", "16px")
+			).ToContainerFragment();
+
+			Assert.Throws<PaddingMustBeFullySpecifiedIfSpecifiedAtAll.PaddingMustBeFullySpecifiedIfSpecifiedAtAllException>(() =>
+			{
+				(new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()).EnsureRulesAreMet(new[] { content });
+			});
+		}
+	}
+}

41cc659741b33afbbdcb21c1e3a07b4d0a3d5c7c
dan <dan@newmind.co.uk>
2013-05-03 18:54:02 +0100

    The RuleEnforcingCssFileLoader will throw a BrokenRuleEncounteredInFileException if a rule is not followed, the message of which indicates which file and where in it the invalid content was encountered

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,4 +1,14 @@
-﻿using UnitTests.Rules;
+﻿using System;
+using System.IO;
+using CSSMinifier.FileLoaders;
+using CSSMinifier.FileLoaders.Factories;
+using CSSMinifier.Logging;
+using CSSMinifier.PathMapping;
+using NonCascadingCSSRulesEnforcer.CSSMinifierIntegration;
+using NonCascadingCSSRulesEnforcer.Rules;
+using CSSParser.ExtendedLESSParser;
+using CSSParser;
+using UnitTests.Rules;
 
 namespace Tester
 {
@@ -6,11 +16,132 @@ namespace Tester
 	{
 		static void Main(string[] args)
 		{
-			(new AllMeasurementsMustBePixelsTests()).HundredPercentageWidthImgsAreNotAcceptableIfNotNestedInPercentageWidthDivs();
-			(new AllMeasurementsMustBePixelsTests()).HundredPercentageWidthImgsMayBeAcceptableIfDeeplyNestedInPercentageWidthDivs();
-			(new AllMeasurementsMustBePixelsTests()).HundredPercentageWidthImgsMayBeAcceptableIfNestedInPercentageWidthDivs();
-			(new AllMeasurementsMustBePixelsTests()).NonHundredPercentageWidthImgsAreNotAcceptableIfNestedInPercentageWidthDivs();
-			(new AllMeasurementsMustBePixelsTests()).PercentageWidthDivsMayBeAcceptable();
+			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).EmptyContentIsAcceptable();
+			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithMediaQueryWrappedStylePropertiesIsNotAcceptable();
+			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithNoNestedContentIsAcceptable();
+			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithOnlyANestedDivIsAcceptable();
+			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithStylePropertiesIsNotAcceptable();
+
+			//(new NoSelectorMayBeRepeatedInTheRules(NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated)).EnsureRulesAreMet(
+			//	LessCssHierarchicalParser.ParseIntoStructuredData(
+			//		Parser.ParseLESS("div.Header, div, div.Footer { div.What1, p, div.What2 { color: red; } } div.Header p { color: blue; }")
+			//	)
+			//);
+
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).EmptyContentIsFine();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).MultipleSelectorsThatAreDifferentAreFine();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).NullInputWillThrowAnException();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorAppearingInDifferentMediaQueriesIsFine();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorAppearingInDifferentNestedMediaQueriesIsFine();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorsWithDifferentStylesIsNotAcceptable();
+			(new NoSelectorMayBeRepeatedInTheRulesTests()).SinglePropertyIsFine();
+
+
+			(new NoSelectorMayBeRepeatedInTheRules(NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated)).EnsureRulesAreMet(
+				LessCssHierarchicalParser.ParseIntoStructuredData(
+					Parser.ParseLESS("div.Header, div.Footer { div.What1, div.What2 { color: red; } }")
+				)
+			);
+
+
+
+			var pathMapper = new FixedPathMapper(@"C:\Users\Me\Documents\Visual Studio 2010\Projects\Blog\Blog\Content");
+			var loader = new RuleEnforcingCssFileLoader(
+				new IEnforceRules[]
+				{
+					new AllMeasurementsMustBePixels(
+						AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthDivs |
+						AllMeasurementsMustBePixels.ConformityOptions.AllowOneHundredPercentOnAnyElementAndProperty
+					),
+					new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheets(),
+					new BorderAndPaddingMayNotBeCombinedWithWidth(),
+					new MarginMustBeFullySpecifiedIfSpecifiedAtAll(),
+					new NoBareSelectorsInNonResetsOrThemeSheets(
+						NoBareSelectorsInNonResetsOrThemeSheets.ScopeRestrictingHtmlTagBehaviourOptions.Allow
+					),
+					new NoMediaQueriesInResetsAndThemeSheets(),
+					new NoSelectorMayBeRepeatedInTheRules(
+						NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated
+					),
+					new OnlyBareSelectorsInResetsAndThemeSheets(
+						OnlyBareSelectorsInResetsAndThemeSheets.ConformityOptions.AllowLessCssMixins
+					),
+					new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()
+				},
+				relativePath =>
+				{
+					var lowerCasedTrimmedRelativePath = relativePath.Trim().ToLower();
+					if (lowerCasedTrimmedRelativePath.EndsWith("resets.css") || lowerCasedTrimmedRelativePath.EndsWith("resets.less"))
+						return StyleSheetTypeOptions.Reset;
+					else if (lowerCasedTrimmedRelativePath.EndsWith("theme.css") || lowerCasedTrimmedRelativePath.EndsWith("theme.less"))
+						return StyleSheetTypeOptions.Themes;
+					else
+						return StyleSheetTypeOptions.Other;
+				},
+				new SimpleTextFileContentLoader(pathMapper),
+				baseContentLoader =>
+					new EnhancedNonCachedLessCssLoaderFactory(
+						baseContentLoader,
+						ErrorBehaviourOptions.LogAndRaiseException,
+						new NullLogger()
+					).Get()
+			);
+			Console.WriteLine(
+				loader.Load("Styles.less").Content
+			);
+			Console.ReadLine();
 		}
+
+		private class FixedPathMapper : IRelativePathMapper
+		{
+			private readonly string _basePath;
+			public FixedPathMapper(string basePath)
+			{
+				if (string.IsNullOrWhiteSpace(basePath))
+					throw new ArgumentException("Null/blank basePath specified");
+
+				_basePath = basePath.Trim();
+			}
+
+			public string MapPath(string relativePath)
+			{
+				if (string.IsNullOrWhiteSpace(relativePath))
+					throw new ArgumentException("Null/blank relativePath specified");
+
+				return Path.Combine(_basePath, relativePath);
+			}
+		}
+
+		/*
+		private class FixedContentTextFileLoader : ITextFileLoader
+		{
+			private TextFileContents[] _files;
+			public FixedContentTextFileLoader(IEnumerable<TextFileContents> files)
+			{
+				if (files == null)
+					throw new ArgumentNullException("files");
+
+				_files = files.ToArray();
+				if (_files.Any(f => f == null))
+					throw new ArgumentException("Null reference encountered in files set");
+			}
+
+			/// <summary>
+			/// This will never return null, it will throw an exception for a null or empty relativePath - it is up to the particular implementation whether or not to throw
+			/// an exception for invalid / inaccessible filenames (if no exception is thrown, the issue should be logged). It is up the the implementation to handle mapping
+			/// the relative path to a full file path.
+			/// </summary>
+			public TextFileContents Load(string relativePath)
+			{
+				if (string.IsNullOrWhiteSpace(relativePath))
+					throw new ArgumentException("Null/blank relativePath specified");
+
+				var file = _files.FirstOrDefault(f => f.RelativePath == relativePath);
+				if (file == null)
+					throw new ArgumentException("No content available for " + relativePath);
+				return file;
+			}
+		}
+		 */
 	}
 }

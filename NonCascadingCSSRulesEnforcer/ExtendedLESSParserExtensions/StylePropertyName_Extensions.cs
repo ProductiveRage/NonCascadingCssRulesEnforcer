b2e23a1be6e37db70ccae9635faf570417edc9a5
dan <742348+ProductiveRage@users.noreply.github.com>
2013-05-04 16:17:43 +0100

    Updated CSSParser reference - the LessCssHierarchicalParser's StylePropertyValue instances contain all value segments for a property instead of just one (eg. "padding: 2px 2px" previously result in one StylePropertyName ("padding") and two StylePropertyValues ("2px" and "2px"), now there will only a single StylePropertyValues with a ValueSegments set of [ "2px", "2px" ])

---
+++ NonCascadingCSSRulesEnforcer/ExtendedLESSParserExtensions/StylePropertyName_Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CSSParser.ExtendedLESSParser;
+
+namespace NonCascadingCSSRulesEnforcer.ExtendedLESSParserExtensions
+{
+	public static class StylePropertyName_Extensions
+	{
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public static bool HasName(this StylePropertyName source, string value)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (value == null)
+				throw new ArgumentNullException("value");
+
+			return source.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase);
+		}
+	}
+}

41bed78dfef738a0c9c9d3a67cb4f7fe8fd41c4d
ProductiveRage <dangger36@gmail.com>
2016-08-13 16:49:06 +0100

    Tidied up the "Tester" project so that it does something that can be executed (validates the style sheets from my blog site, which were written before the validator was created and so there are numerous validation errors!)

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -6,9 +6,7 @@ using CSSMinifier.Logging;
 using CSSMinifier.PathMapping;
 using NonCascadingCSSRulesEnforcer.CSSMinifierIntegration;
 using NonCascadingCSSRulesEnforcer.Rules;
-using CSSParser.ExtendedLESSParser;
-using CSSParser;
-using UnitTests.Rules;
+using NonCascadingCSSRulesEnforcer.Rules.Compatibility;
 
 namespace Tester
 {
@@ -16,103 +14,106 @@ namespace Tester
 	{
 		static void Main(string[] args)
 		{
-			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).EmptyContentIsAcceptable();
-			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithMediaQueryWrappedStylePropertiesIsNotAcceptable();
-			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithNoNestedContentIsAcceptable();
-			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithOnlyANestedDivIsAcceptable();
-			(new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheetsTests()).HtmlTagWithStylePropertiesIsNotAcceptable();
+			// If this was validating style sheets on-the-fly in a web application then an IRelativePathMapper implementation should be used that uses Server.MapPath (or
+			// equivalent) but for the code here, we'll just be reading from the "BlogStyles" folder (which has the style sheets from my Blog - which seems like a nice
+			// compromise; non-trivial but not too large to mentally easily absorb in its entirety)
+			var pathMapper = new FixedPathMapper(basePath: new DirectoryInfo("BlogStyles").FullName);
 
-			//(new NoSelectorMayBeRepeatedInTheRules(NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated)).EnsureRulesAreMet(
-			//	LessCssHierarchicalParser.ParseIntoStructuredData(
-			//		Parser.ParseLESS("div.Header, div, div.Footer { div.What1, p, div.What2 { color: red; } } div.Header p { color: blue; }")
-			//	)
-			//);
+			// This is the full set of validation rules, as described at http://www.productiverage.com/noncascading-css-a-revolution (with a bonus rule added that ensures
+			// that enormous style sheets don't exceed the selector limit for old versions of IE - if that limit is exceeded then the later rules are silently ignored,
+			// which can go unnoticed for some time and be very annoying once discovered!)
+			var fullSetOfValidationRules = new IEnforceRules[]
+			{
+				new AllMeasurementsMustBePixels(
+					AllMeasurementsMustBePixels.ConformityOptions.AllowOneHundredPercentOnAnyElementAndProperty |
+					AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthsOnSpecifiedElementTypes,
+					new[] { "div", "td", "th", "li" }
+				),
+				new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheets(),
+				new BorderAndPaddingMayNotBeCombinedWithWidth(
+					BorderAndPaddingMayNotBeCombinedWithWidth.ConformityOptions.AllowVerticalBorderAndPadding
+				),
+				new MarginMustBeFullySpecifiedIfSpecifiedAtAll(),
+				new NoBareSelectorsInNonResetsOrThemeSheets(
+					NoBareSelectorsInNonResetsOrThemeSheets.ScopeRestrictingHtmlTagBehaviourOptions.Allow
+				),
+				new NoMediaQueriesInResetsAndThemeSheets(),
+				new NoSelectorMayBeRepeatedInTheRules(
+					NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated
+				),
+				new OnlyBareSelectorsInResetsAndThemeSheets(
+					OnlyBareSelectorsInResetsAndThemeSheets.ConformityOptions.AllowLessCssMixins
+				),
+				new PaddingMustBeFullySpecifiedIfSpecifiedAtAll(),
+				new LegacyIESelectorLimitMustBeRespected()
+			};
 
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).EmptyContentIsFine();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).MultipleSelectorsThatAreDifferentAreFine();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).NullInputWillThrowAnException();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorAppearingInDifferentMediaQueriesIsFine();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorAppearingInDifferentNestedMediaQueriesIsFine();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).RepeatedSelectorsWithDifferentStylesIsNotAcceptable();
-			(new NoSelectorMayBeRepeatedInTheRulesTests()).SinglePropertyIsFine();
-
-
-			(new NoSelectorMayBeRepeatedInTheRules(NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated)).EnsureRulesAreMet(
-				LessCssHierarchicalParser.ParseIntoStructuredData(
-					Parser.ParseLESS("div.Header, div.Footer { div.What1, div.What2 { color: red; } }")
-				)
+			// Declare the stylesheet loaded - since this uses the EnhancedNonCachedLessCssLoaderFactory, the @imports will all be flattened and the LESS content processed
+			// into regular CSS. Any validation rules that are broken will be recorded as the process takes place. 
+			var wereAnyRulesBroken = false;
+			var loader = new RuleEnforcingCssFileLoader(
+				rules: fullSetOfValidationRules,
+				styleSheetTypeDeterminer: StyleSheetCategoriser,
+				baseContentLoader: new SimpleTextFileContentLoader(pathMapper),
+				compilerGenerator: baseContentLoader =>
+					new EnhancedNonCachedLessCssLoaderFactory(
+						baseContentLoader,
+						SourceMappingMarkerInjectionOptions.Inject, // This includes extra selectors into the final rules (eg. "#Header.less_37"), a bit like a poor man's source map
+						ErrorBehaviourOptions.LogAndRaiseException,
+						new NullLogger()
+					)
+					.Get(),
+				optionalBrokenFileCallback: brokenRule =>
+				{
+					Console.WriteLine(brokenRule.Message);
+					wereAnyRulesBroken = true;
+				}
 			);
 
-
-            var aux = (new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheets()).GetAnyBrokenRules(
-                LessCssHierarchicalParser.ParseIntoStructuredData(
-                    Parser.ParseLESS("div.Header, div.Footer { div.What1, div.What2 { color: red; } }")
-                )
-            );
-
-
-			//var pathMapper = new FixedPathMapper(@"C:\Users\Me\Documents\Visual Studio 2010\Projects\Blog\Blog\Content");
-            //var pathMapper = new FixedPathMapper(@"D:\CSS\noncascadingcssrulesenforcer\Tester\Content");
-            //var pathMapper = new FixedPathMapper(@"W:\ETWP sites\liverpool\liverpool.etwp.dev.nm\styles");
-            var pathMapper = new FixedPathMapper(@"C:\Users\dpons\Desktop\TestBed");
-            
-            var loader = new RuleEnforcingCssFileLoader(
-                new IEnforceRules[]
-				{
-					new AllMeasurementsMustBePixels(
-						AllMeasurementsMustBePixels.ConformityOptions.AllowOneHundredPercentOnAnyElementAndProperty |
-						AllMeasurementsMustBePixels.ConformityOptions.AllowPercentageWidthsOnSpecifiedElementTypes,
-						new[] { "div", "td", "th", "li" }
-					),
-					new HtmlTagScopingMustBeAppliedToNonResetsOrThemesSheets(),
-					new BorderAndPaddingMayNotBeCombinedWithWidth(
-						BorderAndPaddingMayNotBeCombinedWithWidth.ConformityOptions.AllowVerticalBorderAndPadding
-					),
-					new MarginMustBeFullySpecifiedIfSpecifiedAtAll(),
-					new NoBareSelectorsInNonResetsOrThemeSheets(
-						NoBareSelectorsInNonResetsOrThemeSheets.ScopeRestrictingHtmlTagBehaviourOptions.Allow
-					),
-					new NoMediaQueriesInResetsAndThemeSheets(),
-					new NoSelectorMayBeRepeatedInTheRules(
-						NoSelectorMayBeRepeatedInTheRules.ConformityOptions.AllowBareSelectorsToBeRepeated
-					),
-					new OnlyBareSelectorsInResetsAndThemeSheets(
-						OnlyBareSelectorsInResetsAndThemeSheets.ConformityOptions.AllowLessCssMixins
-					),
-					new PaddingMustBeFullySpecifiedIfSpecifiedAtAll()
-				},
-                relativePath =>
-                {
-                    var lowerCasedTrimmedRelativePath = relativePath.Trim().ToLower();
-                    if (lowerCasedTrimmedRelativePath.EndsWith("resets.css") || lowerCasedTrimmedRelativePath.EndsWith("resets.less"))
-                        return StyleSheetTypeOptions.Reset;
-                    else if (lowerCasedTrimmedRelativePath.EndsWith("theme.css") || lowerCasedTrimmedRelativePath.EndsWith("theme.less")
-                    || lowerCasedTrimmedRelativePath.EndsWith("breakpoints.css") || lowerCasedTrimmedRelativePath.EndsWith("breakpoints.less")
-                    || lowerCasedTrimmedRelativePath.EndsWith("mixinsandvalues.css") || lowerCasedTrimmedRelativePath.EndsWith("mixinsandvalues.less"))
-                        return StyleSheetTypeOptions.Themes;
-                    else
-                        return StyleSheetTypeOptions.Other;
-                },
-                new SimpleTextFileContentLoader(pathMapper),
-                baseContentLoader =>
-                    new EnhancedNonCachedLessCssLoaderFactory(
-                        baseContentLoader,
-                        SourceMappingMarkerInjectionOptions.DoNotInject,
-                        ErrorBehaviourOptions.LogAndRaiseException,
-                        new NullLogger()
-                    ).Get(),
-                    PrintError
-            );
-            Console.WriteLine(
-                loader.Load("Styles.less").Content
-            );
+			// Load and process the example style sheet - if no rules are broken then show the final generated (ie. @import-flattened, LESS-processed, minified) content. If
+			// any validation rules WERE broken then print them out instead.
+			var content = loader.Load("Styles.css");
+			if (!wereAnyRulesBroken)
+			{
+				Console.WriteLine("No rules were broken - hurrah! Press [Enter] to show final content..");
+				Console.ReadLine();
+				Console.WriteLine();
+				Console.WriteLine(content.Content);
+				Console.WriteLine();
+				Console.WriteLine("Press [Enter] to terminate..");
+				Console.ReadLine();
+				return;
+			}
+			Console.WriteLine();
+			Console.WriteLine("Validation complete, press [Enter] to terminate..");
 			Console.ReadLine();
 		}
 
-        static void PrintError(BrokenRuleEncounteredInFileException param)
-        {
-            Console.WriteLine(param.Message);
-        }
+		/// <summary>
+		/// Different types of style sheet require different validation (eg. Reset sheets should only contain bare selectors while most other style sheets should NOT contain
+		/// any bare selectors). The style sheet type is determined by its filename, based upon some conventions which seems to work after applying these rules to various sites.
+		/// Alternative naming schemes could be used, the RuleEnforcingCssFileLoader just needs a delegate that can tell it what style sheet type any given filename should be
+		/// treated as.
+		/// </summary>
+		private static StyleSheetTypeOptions StyleSheetCategoriser(string relativePath)
+		{
+			if (relativePath == null)
+				throw new ArgumentNullException(nameof(relativePath));
+
+			if (relativePath.EndsWith("resets.css", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("resets.less", StringComparison.OrdinalIgnoreCase))
+				return StyleSheetTypeOptions.Reset;
+
+			if (relativePath.EndsWith("theme.css", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("theme.less", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("breakpoints.css", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("breakpoints.less", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("mixinsandvalues.css", StringComparison.OrdinalIgnoreCase)
+			|| relativePath.EndsWith("mixinsandvalues.less", StringComparison.OrdinalIgnoreCase))
+				return StyleSheetTypeOptions.Themes;
+
+			return StyleSheetTypeOptions.Other;
+		}
 
 		private class FixedPathMapper : IRelativePathMapper
 		{
@@ -133,37 +134,5 @@ namespace Tester
 				return Path.Combine(_basePath, relativePath);
 			}
 		}
-
-		/*
-		private class FixedContentTextFileLoader : ITextFileLoader
-		{
-			private TextFileContents[] _files;
-			public FixedContentTextFileLoader(IEnumerable<TextFileContents> files)
-			{
-				if (files == null)
-					throw new ArgumentNullException("files");
-
-				_files = files.ToArray();
-				if (_files.Any(f => f == null))
-					throw new ArgumentException("Null reference encountered in files set");
-			}
-
-			/// <summary>
-			/// This will never return null, it will throw an exception for a null or empty relativePath - it is up to the particular implementation whether or not to throw
-			/// an exception for invalid / inaccessible filenames (if no exception is thrown, the issue should be logged). It is up the the implementation to handle mapping
-			/// the relative path to a full file path.
-			/// </summary>
-			public TextFileContents Load(string relativePath)
-			{
-				if (string.IsNullOrWhiteSpace(relativePath))
-					throw new ArgumentException("Null/blank relativePath specified");
-
-				var file = _files.FirstOrDefault(f => f.RelativePath == relativePath);
-				if (file == null)
-					throw new ArgumentException("No content available for " + relativePath);
-				return file;
-			}
-		}
-		 */
 	}
 }
